e860118899c504c180b7c06dafa7d5d37f8d6ba9
David Berry <dberry3755@gmail.com>
2023-10-16 21:31:54 -0400

    completed part 4

--- catalog/models.py
+++ catalog/models.py
@@ -32,6 +32,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = "Genre"
+
 
 import uuid  # Required for unique book instances
 
@@ -86,3 +92,4 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.last_name}, {self.first_name}"
+
