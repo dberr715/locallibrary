52c6f4544a23398b1ae9510203b0beb41489950a
David Berry <dberry3755@gmail.com>
2023-10-16 22:31:16 -0400

    finished part 6, but error at the last part

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = []
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -11,17 +12,29 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
+
+
+from django.views import generic
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
